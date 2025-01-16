12b9315f7e904ac87adbd4b3e2b880b03c7e8bb8
Swan Htet Aung <swan428ha@prestigein.com>
2025-01-16 20:32:13 +0630

    ratelimit modify

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const { rateLimit } = require("express-rate-limit");
 
 const app = express();
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
 
-// app.set("trust proxy", 1);
-// app.use(limiter);
+app.set("trust proxy", 1);
+app.use(limiter);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
